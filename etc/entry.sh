708a56720bdde5fe37a0f74a44d0621fad9aaaaa
Thomas Obarowski <tobarows@cisco.com>
2022-01-13 17:07:46 +0000

    Adding script and Dockerfile to repo. Still a work in progress, whenever I have time to do so

--- etc/entry.sh
+++ etc/entry.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+#Change to user home dir
 cd {$HOMEDIR}
+
+#Make directory for steam pz server install
 mkdir steam_install
+
+#Use steamcmd to download and install pz dedicated server
 bash ${HOMEDIR}/steamcmd/steamcmd.sh +force_install_dir "{$HOMEDIR}/steam_install" +login anonymous +app_update 380870 +quit
 
 #Make default directory
 mkdir Zomboid/Server
+
 #Download config files and unpack tar.gz.
 wget 
